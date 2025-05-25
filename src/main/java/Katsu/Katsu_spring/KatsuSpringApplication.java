2ddbe6a8c1eb83e8cde6dc5033450381e24e0575
LHE <mekakusira@gmail.com>
2025-05-26 00:43:09 +0900

    5.25. first commit

---
+++ src/main/java/Katsu/Katsu_spring/KatsuSpringApplication.java
@@ -0,0 +1,13 @@
+package Katsu.Katsu_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KatsuSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KatsuSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Katsu/Katsu_spring/KatsuSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package Katsu.Katsu_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KatsuSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
