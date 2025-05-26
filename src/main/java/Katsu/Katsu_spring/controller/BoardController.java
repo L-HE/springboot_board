109e59acdb9581d7f654ecbe11842e9b17944b8d
lhe <gsurplus@inha.edu>
2025-05-27 04:05:27 +0900

    5.27. community version, .yml, insert testing

--- src/main/java/Katsu/Katsu_spring/controller/BoardController.java
+++ src/main/java/Katsu/Katsu_spring/controller/BoardController.java
@@ -3,13 +3,21 @@ package Katsu.Katsu_spring.controller;
 import Katsu.Katsu_spring.dto.BoardDTO;
 import Katsu.Katsu_spring.service.BoardService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @RequiredArgsConstructor
+@Slf4j
 public class BoardController {
     private final BoardService boardService;
+
+    @GetMapping("/save")
+    public String save(){
+        return "save";
+    }
 
     @PostMapping("/save")
     public void save(BoardDTO boardDTO) {
